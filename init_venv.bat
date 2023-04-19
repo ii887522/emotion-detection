80cc1b7c1666f74316e2df4d52521d21775fe16a
ii887522 <ii887522@gmail.com>
2023-04-19 23:08:53 +0800

    commit

---
+++ init_venv.bat
@@ -0,0 +1,5 @@
+@echo off
+
+pip install virtualenv
+python -m virtualenv venv --python=3.10
+venv\Scripts\activate
---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def main():
+    window = tk.Tk()
+    window.title("Emotion Detection")
+    window.geometry("720x720")
+    window.resizable(False, False)
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
