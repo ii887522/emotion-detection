4225a48acb17b10d79e16904d5b09f5f253c08a3
ii887522 <ii887522@gmail.com>
2023-04-20 10:16:40 +0800

    commit

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 
 def main():
+    # How to resize all image datasets so that they are the same sizes ?
+
     window = tk.Tk()
     window.title("Emotion Detection")
     window.geometry("720x720")
